36bcb6c5d29aeee476c13a09d199e7765c85f3ea
Hasan Demir <hsndmr5044@hotmail.com>
2023-05-02 05:08:46 +0300

    feat(docs): add Storybook icon and link

--- src/docs/components/Header/Header.tsx
+++ src/docs/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { MdLightMode, MdOutlineDarkMode } from 'react-icons/all';
+import { MdLightMode, MdOutlineDarkMode, SiStorybook } from 'react-icons/all';
 import Button from '../../../lib/components/Button/Button';
 import useDarkMode from '../../hooks/useDarkMode';
 
@@ -9,7 +9,19 @@ const Header = () => {
     <div className="sticky left-0 top-0 z-50 flex h-14 items-center justify-between border-b border-gray-100 px-4 dark:border-gray-900 dark:bg-gray-950">
       <h1 className="text-xl">Cappadocia UI</h1>
       <div className="flex justify-center">
-        <div>
+        <div className="flex gap-4">
+          <Button
+            as="a"
+            color="gray"
+            className="justify-center"
+            radius="full"
+            customSize={{
+              button: 'w-7 h-7 p-1 text-xl',
+            }}
+            href="https://storybook.cappadocia-ui.com"
+          >
+            <SiStorybook />
+          </Button>
           <Button
             onClick={toggleDarkMode}
             color="gray"
