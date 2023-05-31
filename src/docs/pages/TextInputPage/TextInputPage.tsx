101517940f2efc655d267779fb4bba66d4b55e68
Hasan <41634504+hsndmr@users.noreply.github.com>
2023-06-01 02:12:57 +0300

    feat: add TextInputPage component

---
+++ src/docs/pages/TextInputPage/TextInputPage.tsx
@@ -0,0 +1,11 @@
+import DefaultTextInput from './DefaultTextInput';
+
+const TextInputPage = () => {
+  return (
+    <div className="flex w-full flex-col">
+      <DefaultTextInput />
+    </div>
+  );
+};
+
+export default TextInputPage;
